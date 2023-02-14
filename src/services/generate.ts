d693c519c54c1ec03bbf10ade1414b9149d8ea1a
Dmitry Osipov <dmith.osipov@gmail.com>
2023-02-14 12:25:18 +0300

    udpate uuid

--- src/services/generate.ts
+++ src/services/generate.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'crypto';
+import { randomUUID } from 'node:crypto';
 import { cpSync, createWriteStream, mkdirSync } from 'fs';
 import { join } from 'path';
 import config from '../config';
